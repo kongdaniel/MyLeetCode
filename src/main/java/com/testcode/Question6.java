3a52e8f00e37a8f40448f1fbe532768d08b822e5
LAPTOP-B1RONP0K\xiongkang <2459431805@qq.com>
2024-08-18 18:32:08 +0800

    第6题

---
+++ src/main/java/com/testcode/Question6.java
@@ -0,0 +1,19 @@
+package com.testcode;
+
+public class Question6 {
+
+    public static void main(String[] args) {
+        int [] nums1= {3,2,3};
+        int m = 5;
+
+        Question6 question6=new Question6();
+        question6.rotate(nums1,m);
+//        System.out.println(k);
+        for (int i=0;i<nums1.length;i++){
+            System.out.printf("%d,",nums1[i]);
+        }
+    }
+    public void rotate(int[] nums, int k) {
+
+    }
+}
