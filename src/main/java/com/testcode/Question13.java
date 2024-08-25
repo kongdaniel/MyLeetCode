eec7ea09f575df34075de3017782247cc2b1ed8c
LAPTOP-B1RONP0K\xiongkang <2459431805@qq.com>
2024-08-26 00:08:50 +0800

    做题记录

--- src/main/java/com/testcode/Question13.java
+++ src/main/java/com/testcode/Question13.java
@@ -3,19 +3,34 @@ package com.testcode;
 public class Question13 {
 
     public static void main(String[] args) {
-        int[] nums1 = {3,0,6,1,5};
-        int m = 2;
+        int[] nums1 = {1,2,3,4};
 
         Question13 question = new Question13();
-         k = question.productExceptSelf(nums1);
-        System.out.println(k);
-//        for (int i=0;i<nums1.length;i++){
-//            System.out.printf("%d,",nums1[i]);
-//        }
+         int [] result = question.productExceptSelf(nums1);
+        for (int i=0;i<result.length;i++){
+            System.out.printf("%d,",result[i]);
+        }
     }
 
     public int[] productExceptSelf(int[] nums) {
 
-        return 1;
+        int n = nums.length;
+        int productArr [] =new int[n];
+        int reverseProductArr []=new int[n];
+        productArr[0] = nums[0];
+        for (int i = 1; i < n ; i++) {
+            productArr[i] = productArr[i-1] * nums[i];
+        }
+        reverseProductArr[n-1] = nums[n-1];
+        for (int i=n-2; i >= 0; i--) {
+            reverseProductArr[i] = reverseProductArr[i+1] * nums[i];
+        }
+        int [] result = new int[n];
+        result[0]=reverseProductArr[1];
+        result[n-1]=productArr[n-2];
+        for (int i = 1; i <= n-2; i++) {
+            result[i] = productArr[i-1]*reverseProductArr[i+1];
+        }
+        return result;
     }
 }
